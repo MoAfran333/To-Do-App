bdbef1280a1bd763200a227964d09ffe3d37a9d3
MoAfran333 <moafran123@gmail.com>
2024-10-02 19:11:59 +0530

    Added User Email to the HomePage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -22,20 +22,24 @@ const HomePage = () => {
       <div className="h-screen w-full p-12 mx-10 my-8 flex flex-col items-center">
         {currentUser?.email !== null && currentUser?.email !== undefined ? (
           <div className="w-full">
-            <div className="flex justify-end">
-              <button
-                type="button"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-              >
-                <Link to={"/create"}>Add a Task</Link>
-              </button>
-              <button
-                type="button"
-                onClick={handleLogout}
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-              >
-                LogOut
-              </button>
+            <div className="flex justify-between text-white text-xl font-bold">
+              {currentUser?.email}
+
+              <div className="flex">
+                <button
+                  type="button"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                >
+                  <Link to={"/create"}>Add a Task</Link>
+                </button>
+                <button
+                  type="button"
+                  onClick={handleLogout}
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                >
+                  LogOut
+                </button>
+              </div>
             </div>
             <TODO todoList={currentUser?.todoList} />
           </div>
